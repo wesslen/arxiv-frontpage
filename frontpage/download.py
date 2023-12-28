fde5d143c9ea1b1ac0f1bb5fbe8817c0f197fac2
Ryan Wesslen <ryan@explosion.ai>
2023-12-28 12:59:31 -0500

    enhance download.py

--- frontpage/download.py
+++ frontpage/download.py
@@ -1,71 +1,73 @@
-import datetime as dt 
+import datetime as dt
+import logging
 from pathlib import Path
-from typing import List
-
 import srsly
 import tqdm
 import arxiv
-from arxiv import Result
-from retry import retry 
+from retry import retry
 import spacy
-from spacy.language import Language
-from .types import ArxivArticle
-from rich.console import Console 
 
-console = Console()
+# Assuming the ArxivArticle is defined with properties
+# `created`, `title`, `abstract`, `sentences`, and `url`.
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def age_in_days(res: Result) -> float:
-    """Get total seconds from now from Arxiv result"""
+def get_article_age_in_days(article_published_date):
+    """Calculate the age of an article in days."""
     now = dt.datetime.now(dt.timezone.utc)
-    return (now - res.published).total_seconds() / 3600 / 24
+    return (now - article_published_date).total_seconds() / (3600 * 24)
 
-
-def parse(res: Result, nlp: Language) -> ArxivArticle:
-    """Parse proper Pydantic object from Arxiv"""
-    summary = res.summary.replace("\n", " ")
+def parse_article_result_to_dict(article_result, nlp):
+    """Parse arxiv.Result to dictionary representing ArxivArticle."""
+    summary = article_result.summary.replace("\n", " ")
     doc = nlp(summary)
-    sents = [s.text for s in doc.sents]
+    sentences = [s.text for s in doc.sents]
     
-    return ArxivArticle(
-        created=str(res.published)[:19], 
-        title=str(res.title),
-        abstract=summary,
-        sentences=sents,
-        url=str(res.entry_id)
-    )
+    return {
+        "created": str(article_result.published)[:19],
+        "title": str(article_result.title),
+        "abstract": summary,
+        "sentences": sentences,
+        "url": str(article_result.entry_id)
+    }
 
 @retry(tries=5, delay=1, backoff=2)
-def main():
-    nlp = spacy.load("en_core_web_sm", disable=["ner", "lemmatizer", "tagger"])
-    console.log(f"Starting arxiv search.")
-    items = arxiv.Search(
+def fetch_and_filter_articles(nlp, query="cs", max_results=200, days_limit=2.5):
+    """Fetch and filter articles from Arxiv."""
+    search_results = arxiv.Search(
         query="and",
-        max_results=200,
+        max_results=max_results,
         sort_by=arxiv.SortCriterion.SubmittedDate,
     )
+    
+    results = [result for result in search_results.results() 
+               if get_article_age_in_days(result.published) < days_limit 
+               and result.primary_category.startswith(query)]
+    logger.info(f"Found {len(results)} new results within {days_limit} days in the {query} category.")
+    return results
 
-    results = list(items.results())
-
-    console.log(f"Found {len(results)} results.")
-
-    articles = [dict(parse(r, nlp=nlp)) 
-                for r in tqdm.tqdm(results) 
-                if age_in_days(r) < 2.5 and r.primary_category.startswith("cs")]
-
-    dist = [age_in_days(r) for r in results]
-    console.log(f"Minimum article age: {min(dist)}")
-    console.log(f"Maximum article age: {max(dist)}")
-    articles_dict = {ex['title']: ex for ex in articles}
-    most_recent = list(sorted(Path("data/downloads/").glob("*.jsonl")))[-1]
-    old_articles_dict = {ex['title']: ex for ex in srsly.read_jsonl(most_recent)}
-
-    new_articles = [ex for title, ex in articles_dict.items() if title not in old_articles_dict.keys()]
-    old_articles = [ex for title, ex in articles_dict.items() if title in old_articles_dict.keys()]
-    if old_articles:
-        console.log(f"Found {len(old_articles)} old articles in current batch. Skipping.")
+def save_new_articles(new_articles):
+    """Save new articles in the data/downloads directory."""
     if new_articles:
-        console.log(f"Found {len(new_articles)} new articles in current batch to write.")
-        filename = str(dt.datetime.now()).replace(" ", "-")[:13] + "h.jsonl"
-        srsly.write_jsonl(Path("data") / "downloads" / filename, new_articles)
-        console.log(f"Wrote {len(new_articles)} articles into {filename}.")
+        filename = f"{dt.datetime.now().replace(microsecond=0).isoformat()}-articles.jsonl"
+        srsly.write_jsonl(Path("data/downloads") / filename, new_articles)
+        logger.info(f"Wrote {len(new_articles)} new articles into {filename}.")
+
+def main():
+    nlp = spacy.load("en_core_web_sm", disable=["ner", "lemmatizer", "tagger"])
+    
+    fetched_articles = fetch_and_filter_articles(nlp=nlp) # added default parameters
+    
+    parsed_articles = [parse_article_result_to_dict(article, nlp=nlp) for article in tqdm.tqdm(fetched_articles)]
+    
+    # load previously saved articles to check for duplicates
+    most_recent_file = sorted(Path("data/downloads").glob("*.jsonl"))[-1]
+    old_articles_dict = {art['title']: art for art in srsly.read_jsonl(most_recent_file)}
+    
+    new_articles = [art for art in parsed_articles if art['title'] not in old_articles_dict]
+    
+    save_new_articles(new_articles)
+
+if __name__ == "__main__":
+    main()
